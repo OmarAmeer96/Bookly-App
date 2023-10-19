fe527af353ac57b4f92ec59a730c119b74323539
Omar_Ameer <omar.ameer244@gmail.com>
2023-10-19 21:13:58 +0300

    Responsive fro ListViewItem Done.

--- lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -9,16 +9,20 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: Responsive.screenWidth(context) * (150 / 224),
-      height: 224,
-      decoration: BoxDecoration(
-        image: const DecorationImage(
-          image: AssetImage(AssetsData.testImage),
-          fit: BoxFit.fill,
+    return SizedBox(
+      width: Responsive.screenWidth(context) * 0.36,
+      child: AspectRatio(
+        aspectRatio: 150 / 224,
+        child: Container(
+          decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: AssetImage(AssetsData.testImage),
+              fit: BoxFit.fill,
+            ),
+            borderRadius: BorderRadius.circular(18),
+            color: Colors.deepOrange,
+          ),
         ),
-        borderRadius: BorderRadius.circular(18),
-        color: Colors.deepOrange,
       ),
     );
   }

9a80ec3bee09a84bfc50faada9e13a40335f8390
Omar_Ameer <omar.ameer244@gmail.com>
2023-10-19 16:57:53 +0300

    Added the SlidingText Animation.

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,34 @@
 import 'package:bookly_app/Core/utils/assets.dart';
+import 'package:bookly_app/Features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,10 +43,8 @@ class SplashViewBody extends StatelessWidget {
         const SizedBox(
           height: 20,
         ),
-        const Text(
-          "Read Free Books!",
-          textAlign: TextAlign.center,
-        ),
+        // Equal to SetState, But updating the current widget only.
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }

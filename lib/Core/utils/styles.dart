7b0eee945cc7f7bccd15c7cec55ce0df486ad69c
Omar_Ameer <omar.ameer244@gmail.com>
2023-10-20 15:10:40 +0300

    Added the Styles Class.

---
+++ lib/Core/utils/styles.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const montserratRegular = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Montserrat-Regular',
+  );
+  static const montserratMedium = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Montserrat-Medium',
+  );
+  static const montserratSemiBold = TextStyle(
+    fontSize: 20,
+    fontFamily: 'Montserrat-SemiBold',
+  );
+  static const montserratBold = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Montserrat-Bold',
+  );
+  static const gtSectraFineRegular = TextStyle(
+    fontSize: 22,
+    fontFamily: 'GT-Sectra-Fine-Regular',
+  );
+}
